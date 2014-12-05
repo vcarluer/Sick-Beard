e9f12050ac8badc8ad764aa85f5f8904541a9215
vcarluer <vcarluer@gmail.com>
2014-12-05 01:57:18 +0100

    torrent to sick beard. Hook used in deluge execute plugin

---
+++ autoProcessTV/torrentToSickBeard.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+# Author: Vincent Carluer <vcarluer@gmail.com>
+# URL: http://code.google.com/p/sickbeard/
+#
+# This file is part of Sick Beard.
+#
+# Sick Beard is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Sick Beard is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Sick Beard.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import sys
+
+try:
+    import autoProcessTV
+except:
+    print ("Can't import autoProcessTV.py, make sure it's in the same folder as " + sys.argv[0])
+    sys.exit(1)
+    
+# 0  sys.argv[0] is the final directory of the job (full path)
+if len(sys.argv) < 1:
+    print ("No folder supplied")
+    sys.exit(1)
+else:
+    download_final_dir = sys.argv[0]
+    
+# Only final_dir and org_NZB_name are being used to process episodes
+autoProcessTV.processEpisode(download_final_dir, None)
